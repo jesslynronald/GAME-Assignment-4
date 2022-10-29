7aa8acf5d0332fab91eefbf6b520c1f159ce671f
jesslynronald <jesslynronald@gmail.com>
2022-10-30 06:03:07 +1100

    Final commit (credit)

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasManager : MonoBehaviour
+{
+    public Button button1;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void FinalGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
+public class PacStudentController : MonoBehaviour
 {
     public Animator animator;
     private Vector3 movement;
     private float movementSqrMagnitude;
     private GameObject player;
+    private string lastInput;
+    private string currentInput;
+    private string playerInput = "";
 
     // Start is called before the first frame update
     void Start()
@@ -21,41 +24,42 @@ public class InputManager : MonoBehaviour
         GetMovementVector();
         PlayerPosition(player);
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             animator.SetBool("Up", true);
             animator.SetBool("Down", false);
             animator.SetBool("Left", false);
             animator.SetBool("Right", false);
-            Debug.Log("Up");
-        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+            lastInput = "W";            
+        } else if (Input.GetKeyDown(KeyCode.S))
         {
             animator.SetBool("Down", true);
             animator.SetBool("Up", false);
             animator.SetBool("Left", false);
             animator.SetBool("Right", false);
-            Debug.Log("Down");
-        } else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            lastInput = "S";
+        } else if (Input.GetKeyDown(KeyCode.A))
         {
             animator.SetBool("Left", true);
             animator.SetBool("Down", false);
             animator.SetBool("Up", false);
             animator.SetBool("Right", false);
-            Debug.Log("Left");
-        } else if (Input.GetKeyDown(KeyCode.RightArrow))
+            lastInput = "A";
+        } else if (Input.GetKeyDown(KeyCode.D))
         {
             animator.SetBool("Right", true);
             animator.SetBool("Down", false);
             animator.SetBool("Left", false);
             animator.SetBool("Up", false);
-            Debug.Log("Right");
+            lastInput = "D";           
         }
+        // Debug.Log(lastInput);
     }
 
     void GetMovementVector()
     {
-        movement.x = Input.GetAxis("Horizontal") * 0.01f;
-        movement.y = Input.GetAxis("Vertical") * 0.01f;
+        movement.x = Input.GetAxis("Horizontal") * 0.07f;
+        movement.y = Input.GetAxis("Vertical") * 0.07f;
         movement = Vector3.ClampMagnitude(movement, 1.0f);
         movementSqrMagnitude = movement.sqrMagnitude;
     }
